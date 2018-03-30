6c2d98e146fb5481ffe40e9e05d4ecf83ed4aea9
Kevin Kane <kkane@marvin.wesleyan.edu>
2018-03-30 11:11:34 -0400

    adding mysql connection

--- nba_draft.py
+++ nba_draft.py
@@ -1,8 +1,16 @@
 from flask import Flask, current_app, render_template
+import pymysql
+
 app = Flask(__name__)
+
+def getSQLDatabaseConn():
+    conn = pymysql.connect( host='127.0.0.1', user='csc545_final', passwd='nb4f4nt4sy', db='csc545_final' )
+    return conn
 
 @app.route("/")
 def index():
+    conn = getSQLDatabaseConn()
+    
     return render_template( 'index.html' )
 
 @app.route( "/static/<fileName>" )
